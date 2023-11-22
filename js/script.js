eef4fd977f714ef3a3d7417ca77e7023988be33a
Jacó Apolinário <53705089+Jacoappolinario@users.noreply.github.com>
2023-11-22 18:27:18 -0300

    comment JavaScript code

--- js/script.js
+++ js/script.js
@@ -1,43 +1,43 @@
 // TODO: Refazer toda essa lógica, levando em conta a nova estrutura html. Mas usar essa de exemplo.
-const values = document.querySelectorAll(".rating-list .rating-button");
+// const values = document.querySelectorAll(".rating-list .rating-button");
 
-values.forEach((value) => {
-  value.addEventListener("click", (event) => {
-    const selectedValue = event.currentTarget;
+// values.forEach((value) => {
+//   value.addEventListener("click", (event) => {
+//     const selectedValue = event.currentTarget;
 
-    selectedValue.classList.add("active");
+//     selectedValue.classList.add("active");
 
-    values.forEach((value) => {
-      if (value !== selectedValue) {
-        value.classList.remove("active");
-      }
-    });
+//     values.forEach((value) => {
+//       if (value !== selectedValue) {
+//         value.classList.remove("active");
+//       }
+//     });
 
-    const hiddenField = document.getElementById("selectedValue");
+//     const hiddenField = document.getElementById("selectedValue");
 
-    hiddenField.value = +selectedValue.innerText;
-  });
-});
+//     hiddenField.value = +selectedValue.innerText;
+//   });
+// });
 
-const form = document.querySelector(".rating-form");
+// const form = document.querySelector(".rating-form");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
 
-  const button = document.querySelector(".submit-button");
-  button.disabled = true;
-  button.innerText = "Enviando...";
+//   const button = document.querySelector(".submit-button");
+//   button.disabled = true;
+//   button.innerText = "Enviando...";
 
-  const data = new FormData(form);
+//   const data = new FormData(form);
 
-  fetch("./enviar.php", {
-    method: "POST",
-    body: data,
-  }).then((response) => {
-    if (response.ok) {
-      form.innerHTML = "<p>OK</p>";
-    } else {
-      form.innerHTML = "<p>Erro</p>";
-    }
-  });
-});
+//   fetch("./enviar.php", {
+//     method: "POST",
+//     body: data,
+//   }).then((response) => {
+//     if (response.ok) {
+//       form.innerHTML = "<p>OK</p>";
+//     } else {
+//       form.innerHTML = "<p>Erro</p>";
+//     }
+//   });
+// });
